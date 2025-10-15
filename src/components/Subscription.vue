ffdeb9dcca36bb77c230b60f75a4c35791caa706
vol4tim <vol4tim@gmail.com>
2025-10-15 11:00:59 +0400

    add devices when purchasing a subscription

--- src/components/Subscription.vue
+++ src/components/Subscription.vue
@@ -1,19 +1,36 @@
 <script setup>
 import { useAccount, useSend } from "robonomics-interface-vue/account";
 import {
+  useAction as useActionDevices,
+  useDevices
+} from "robonomics-interface-vue/devices";
+import {
   useAction,
   useAvailableSubscriptions,
   useSubscription
 } from "robonomics-interface-vue/subscription";
+import { useAction as useActionTools } from "robonomics-interface-vue/tools";
+import { ref } from "vue";
 
 const { account } = useAccount();
 const { loading, error, data } = useSubscription(account);
 const { data: freeAuctions } = useAvailableSubscriptions();
 const action = useAction();
 const { tx } = useSend();
+const { batch } = useActionTools();
+const actionDevices = useActionDevices();
+const { data: dataDevices } = useDevices(account);
+const addressAltruist = ref("");
 
 const click = async () => {
-  await tx.send(async () => await action.bid(1000000001));
+  const calls = [await action.bid(1000000001)];
+  const addresses = [account.value, addressAltruist.value].filter(
+    item => !dataDevices.value.includes(item) && addressAltruist.value
+  );
+  if (addresses.length > 0) {
+    calls.push(actionDevices.add(dataDevices.value, addresses));
+  }
+  await tx.send(() => batch(calls));
 };
 </script>
 
@@ -30,6 +47,10 @@ const click = async () => {
           <code>
             <pre>{{ data }}</pre>
           </code>
+          <b>Devices</b>
+          <code>
+            <pre>{{ dataDevices }}</pre>
+          </code>
         </div>
         <div v-else>You need to select an account.</div>
       </div>
@@ -39,6 +60,7 @@ const click = async () => {
     <div>
       <h2>Buy</h2>
       <div>free auctions: {{ freeAuctions }}</div>
+      <input v-model="addressAltruist" placeholder="Address altruist" />
       <button @click="click">buy</button>
       <div>
         <div>process: {{ tx.process }}</div>
